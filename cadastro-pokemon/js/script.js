e2118b12d2dd6cae2738dcb2693f9c0c86eae97b
victorkoba <victorkoba08@gmail.com>
2025-07-29 10:17:09 -0300

    commit teste 5

--- cadastro-pokemon/js/script.js
+++ cadastro-pokemon/js/script.js
@@ -1,4 +1,16 @@
+// Abrir/fechar menu ao clicar no botão
 function toggleMenu() {
   const menu = document.getElementById("menuLateral");
   menu.classList.toggle("ativo");
 }
+
+// Fechar o menu ao clicar fora
+document.addEventListener("click", function(event) {
+  const menu = document.getElementById("menuLateral");
+  const botao = document.querySelector(".hamburguer");
+
+  // Se o menu estiver aberto E o clique for fora do menu e fora do botão
+  if (menu.classList.contains("ativo") && !menu.contains(event.target) && !botao.contains(event.target)) {
+    menu.classList.remove("ativo");
+  }
+});
